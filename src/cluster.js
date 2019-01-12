bef031e20fecda4038528d1732e2eba180dc87ab
Kevin Simper <kevin.simper@gmail.com>
2019-01-12 10:23:09 +0100

    Easily choose another project if wrong project

--- src/cluster.js
+++ src/cluster.js
@@ -1,27 +1,49 @@
 #!/usr/bin/env node
 
-const { spawn, execSync } = require("child_process");
+const { fork, spawn, execSync } = require("child_process");
 const prompts = require("prompts");
+
+function getCredentials(cluster) {
+  let cmd = `gcloud container clusters get-credentials --region=${
+    cluster.zone
+  } ${cluster.name}`;
+  console.log(cmd);
+  spawn(cmd, {
+    shell: true,
+    stdio: "inherit"
+  });
+}
+
+function chooseProject() {
+  spawn("./project.js", {
+    stdio: "inherit"
+  }).on("exit", () => {
+    main();
+  });
+}
 
 async function main() {
   const clusters = JSON.parse(
     execSync("gcloud container clusters list --format=json")
   );
   const choices = clusters.map((c, id) => ({ title: c.name, value: id }));
+  choices.push({
+    title: "→ Choose other project",
+    value: "other"
+  });
   const response = await prompts({
     type: "select",
     name: "value",
     message: "Pick a cluster",
     choices
   });
-  let cmd = `gcloud container clusters get-credentials --region=${
-    clusters[response.value].zone
-  } ${clusters[response.value].name}`;
-  console.log(cmd);
-  spawn(cmd, {
-    shell: true,
-    stdio: "inherit"
-  });
+
+  switch (response.value) {
+    case "other":
+      return chooseProject();
+    default:
+      return getCredentials(clusters[response.value]);
+  }
 }
 
 main();
